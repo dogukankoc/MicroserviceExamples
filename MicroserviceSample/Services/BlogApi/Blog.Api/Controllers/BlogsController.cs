cb82891e106c78fbee725e85fe110c0a8c7e6a19
Dogukan Koc <dk.dogukankoc@gmail.com>
2023-08-10 21:37:55 +0300

    BasicMicroserviceSampleCreated

---
+++ MicroserviceSample/Services/BlogApi/Blog.Api/Controllers/BlogsController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Blog.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BlogsController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Blogs() 
+        {
+            return Ok("Request Success");
+        }
+    }
+}
---
+++ MicroserviceSample/Services/UserApi/User.Api/Controllers/UsersController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace User.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Users ()
+        {
+            return Ok("Success");
+        }
+    }
+}
